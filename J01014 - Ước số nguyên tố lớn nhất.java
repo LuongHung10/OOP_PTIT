02ef53d17c560dfaea7d5fb11c777fb881b878cf
LuongHung10 <142741591+LuongHung10@users.noreply.github.com>
2023-09-18 21:01:11 +0700

    Create J01014 - Ước số nguyên tố lớn nhất.java

---
+++ J01014 - Ước số nguyên tố lớn nhất.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- != 0)
+        {
+             long n = sc.nextLong();
+             long res = 0;
+             for(int i = 2; i <= Math.sqrt(n); i++)
+             {
+                 if(n % i == 0)
+                 {
+                     while(n % i == 0)
+                     {
+                         n /= i;
+                     }
+                     res = Math.max(res, i);
+                 }
+             }
+             res = Math.max(res, n);
+             System.out.println(res);
+        }
+    }
+}
